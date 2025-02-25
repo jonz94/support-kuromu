0bbec35cb85912de42e133b27a8db5f696b220fc
jonz94 <jody16888@gmail.com>
2025-02-25 23:40:31 +0800

    fix: remark is required

--- components/payment-form.tsx
+++ components/payment-form.tsx
@@ -85,7 +85,7 @@ export function PaymentForm() {
                   </label>
 
                   <label className="relative flex cursor-pointer flex-col items-center gap-3 rounded-lg border border-input px-2 py-3 text-center shadow-sm shadow-black/5 outline-offset-2 transition-colors has-[[data-state=checked]]:border-2 has-[[data-state=checked]]:border-ring has-[:focus-visible]:outline has-[:focus-visible]:outline-2 has-[:focus-visible]:outline-ring/70">
-                    <RadioGroupItem value="opay" className="sr-only after:absolute after:inset-0" />
+                    <RadioGroupItem value="opay" className="sr-only after:absolute after:inset-0" disabled />
                     <picture className="grid h-20 w-auto place-content-center">
                       <img src="/opay-logo-outline.png" alt="opay logo" />
                     </picture>
